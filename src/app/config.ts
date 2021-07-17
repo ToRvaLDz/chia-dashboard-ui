d81f537e84b5a7261f3f2d752f997d051674ba7a
Marco Migozzi <torvaldz@gmail.com>
2021-07-17 19:21:20 +0200

    testing oauth

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
-export const discordClientId = '';
+export const discordClientId = '863480056024924201';
 export const githubClientId = '';
 export const googleClientId = '';
-export const apiBaseUrl = 'https://us.chiadashboard.com/api';
+export const apiBaseUrl = 'https://chia-dashboard-ui.pages.dev/api';
 
 export const enablePeriodicUpdates = true;
 export const requestDiscordGuildPermission = true;
